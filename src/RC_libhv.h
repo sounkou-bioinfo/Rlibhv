774edc3b8b2da4848f398c24c5080a5bda0f576c
sounkou-bioinfo <sounkoutoure@gmail.com>
2025-06-12 20:51:44 +0400

    some cleanup attemp configure : cleanup and make it clear R C level: rename some files for consitent naming install.libs.R : styling configure.win : will this work ?

---
+++ src/RC_libhv.h
@@ -0,0 +1,23 @@
+#ifndef RLIBHV_H
+#define RLIBHV_H
+
+#define R_NO_REMAP
+#include <R.h>
+#include <Rinternals.h>
+#include <Rdefines.h>
+
+/* libhv includes */
+#include "hv.h"
+#include "hloop.h"
+#include "hsocket.h"
+#include "hssl.h"
+
+/* R wrapper functions for libhv */
+SEXP R_libhv_version();
+SEXP R_libhv_compile_version();
+SEXP R_libhv_version_number();
+
+/* TCP echo server function */
+SEXP R_tcp_echo_server(SEXP port_sexp);
+
+#endif /* RLIBHV_H */
